7bcf2fbd22b22df3f7a31e34a351f650c7850740
syllfarney-max <syllfarney@gmail.com>
2025-10-10 06:52:42 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,11 @@ export default function App() {
   return (
     <div className="text-center p-8">
       <img src="/comurg.jpg" alt="Logo Comurg" className="mx-auto w-16 h-16 mb-4" />
+<img 
+  src="/comurg.jpg" 
+  alt="Logo Comurg" 
+  className="mx-auto mb-2 w-16 h-auto opacity-90"
+/>
       <h1 className="text-4xl font-bold text-green-700">Viveiros ® Comurg</h1>
       <p className="text-lg mt-2 text-gray-700">
         Mudas para paisagismo, recuperação ambiental e projetos de reflorestamento.
