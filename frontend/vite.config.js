75c5e2f9b0fd0131e61a2f049db87944a32c7ffa
syllfarney-max <syllfarney@gmail.com>
2025-10-12 01:17:06 -0300

    Delete frontend directory

--- frontend/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// 🔧 Corrige o caminho base para produção e local
-export default defineConfig({
-  plugins: [react()],
-  base: './', // ✅ garante que arquivos públicos (ex: /comurg.jpg) funcionem no Render
-})
