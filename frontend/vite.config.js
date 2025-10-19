a390c4af4304f3dee35f0c50bff6297c71ebbf10
syllfarney-max <syllfarney@gmail.com>
2025-10-19 20:06:44 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-  build: {
-    outDir: "dist",
+  define: {
+    "process.env": process.env,
   },
   server: {
-    port: 5173,
+    proxy: {
+      "/api": {
+        target: "https://viveiro-comurg-backend-yjsj.onrender.com",
+        changeOrigin: true,
+        secure: true,
+      },
+    },
   },
 });
