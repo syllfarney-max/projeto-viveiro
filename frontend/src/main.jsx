83089e53d2ad6dcde979b8fcac64da55b628571f
syllfarney-max <syllfarney@gmail.com>
2025-10-21 21:01:09 -0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./style.css"; // 🔹 ESSENCIAL - o arquivo faltava
+import "./styles.css"; // seu arquivo de estilos já existente
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
